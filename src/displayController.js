b70ef436c44f655eb29027b6d133fcc74416b3eb
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-10-26 12:11:24 -0500

    Agrega parametro y funcion para crear el display de un To do

--- src/displayController.js
+++ src/displayController.js
@@ -1,14 +1,26 @@
 //Toma el objeto a crear y devuelve su informacion como un elemento html
-function createElement(todoObj){
+function createElement(todoObj,id){
     let todoElement;
     if(todoObj.type ==="todo"){
-        todoElement = createTodoDisplay(todoObj);
+        todoElement = createTodoDisplay(todoObj,id);
     }else if(todoObj.type === "note"){
-        todoElement = createNoteDisplay(todoObj);
+        todoElement = createNoteDisplay(todoObj,id);
     }else if(todoObj.type ==="checkList"){
-        todoElement = createCheckListDisplay(todoObj);
+        todoElement = createCheckListDisplay(todoObj,id);
     }else{
         todoElement = createErrorDisplay();
     }
     return todoElement;
+}
+
+function createTodoDisplay(todoObj,id){
+    let todoDisplay = document.createElement("div");
+    todoDisplay.classList.add("todo");
+    todoDisplay.setAttribute("data-id",id);
+
+    todoDisplay.appendChild(createTodoCheckBtn());
+    todoDisplay.appendChild(createTodoTop());
+    todoDisplay.appendChild(createTodoDescription());
+
+    return todoDisplay;
 }
