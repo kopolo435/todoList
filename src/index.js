ef53454af8d19bb4a96ac99617b0fc96bb7afb07
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-10-27 16:51:15 -0500

    Agrega imports y declaracion de variables del html

--- src/index.js
+++ src/index.js
@@ -3,36 +3,16 @@ import * as todoObjectsController  from "./todoObjectsController.js";
 import createNote from "./noteObject.js";
 import * as checkList from "./checkListObject.js";
 import style from "./style.css";
-
-
-
-
-function createObject(object){
-    if(object.type === "todo"){
-        return (createTodo(object.title,object.description,
-            object.dueDate,object.status,object.priority,object.project)
-            );
-    } else if(object.type ==="checkList"){
-        return (checkList.createCheckList(object.title,object.checkList,object.dueDate,
-            object.priority,object.project));
-    }else if(object.type ==="note"){
-        return (createNote(object.title,object.noteText,object.lastModifiedDate,
-            object.priority));
-    }
-}
-
-function createChecks(stringListCheck){
-  let newCheckList = [];
-  stringListCheck.forEach(element => {
-      newCheckList.push(checkList.createCheck(element));
-  });
-  return newCheckList;
-}
-
+import testArray from "../testInput.js"
 import createElement from './displayController.js'
 
-let addBtn = document.getElementById("addBtn");
-let projectsContainer = document.getElementById("todoContainer");
-projectsContainer.appendChild(createElement(note2,1));
+
+const addBtn = document.getElementById("addBtn");
+const projectsContainer = document.getElementById("todoContainer");
+
+
+function updateShownProjects(currentProject,projectsArray){
+
+}
 
 
