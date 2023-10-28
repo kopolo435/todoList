9cd16f0d824dedc03455e500965144965e69e5cd
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-10-28 12:15:25 -0500

    Agrega funcion para crear label de titulo

--- src/addTaskDisplay.js
+++ src/addTaskDisplay.js
@@ -45,3 +45,22 @@ function createChecklistDisplay(){
 
     return elementsArray;
 }
+
+function createTitleLabel(){
+    const label = document.createElement("label");
+    label.setAttribute("for","createTitle");
+    label.classList.add("topLabel");
+    label.textContent = "Nombre Tarea";
+    label.appendChild(createTitle());
+
+    const createTitle = ()=>{
+        const title = document.createElement("input");
+        title.setAttribute("type","text");
+        title.setAttribute("name","title");
+        title.setAttribute("id","createTitle");
+        title.setAttribute("placeholder","Nombre Tarea");
+
+        return title
+    }
+    return label;
+}
