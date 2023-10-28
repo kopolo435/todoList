eaf195181aba5e485a398c0981303c93f5b9547e
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-10-28 13:38:43 -0500

    Agrega evento para modificar lo que se muestra en el modal

--- src/index.js
+++ src/index.js
@@ -5,18 +5,19 @@ import * as checkList from "./checkListObject.js";
 import style from "./style.css";
 import testArray from "../testInput.js"
 import createElement from './displayController.js'
-
+import addTaskDisplay from './addTaskDisplay.js'
 
 const addBtn = document.getElementById("addBtn");
 const projectsContainer = document.getElementById("todoContainer");
 const completedProjectsContainer = document.getElementById("completedContainer")
+const taskTypeBtn = document.getElementById("selectType");
+const modalForm = document.getElementById("createHeader");
 
 let currentProject = "projecto1";
 let projectsArray = testArray;
 function updateShownProjects(){
     let currentProjectArray = projectsArray.filter(task => task.project === currentProject)
     currentProjectArray.forEach((taskObj,index) => {
-        console.log("tipo:"+taskObj.type)
         if(!taskObj.status){
             projectsContainer.appendChild(createElement(taskObj,index));
         }
@@ -26,5 +27,12 @@ function updateShownProjects(){
     });
 }
 updateShownProjects();
+
+taskTypeBtn.addEventListener("change",e =>{
+    let elementsArray = addTaskDisplay(e.target.value);
+    elementsArray.forEach(item =>{
+        modalForm.appendChild(item);
+    })
+})
 
 
