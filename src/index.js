ac46c67ee72379503339da3ff46f924af2ea8301
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-10-29 13:40:20 -0500

    Agrega funcion para crear todo obj

--- src/index.js
+++ src/index.js
@@ -12,11 +12,14 @@ const projectsContainer = document.getElementById("todoContainer");
 const completedProjectsContainer = document.getElementById("completedContainer")
 const taskTypeBtn = document.getElementById("selectType");
 const modalForm = document.getElementById("createHeader");
+const saveChangesBtn = document.getElementById("saveChanges");
 
 let currentProject = "projecto1";
 let projectsArray = testArray;
 function updateShownProjects(){
     let currentProjectArray = projectsArray.filter(task => task.project === currentProject)
+    projectsContainer.replaceChildren();
+    completedProjectsContainer.replaceChildren();
     currentProjectArray.forEach((taskObj,index) => {
         if(!taskObj.status){
             projectsContainer.appendChild(createElement(taskObj,index));
@@ -36,4 +39,21 @@ taskTypeBtn.addEventListener("change",e =>{
     })
 })
 
+saveChangesBtn.addEventListener("click", e=>{
+    if(taskTypeBtn.value === "todo"){
+        projectsArray.push(callCreateTodo());
+        console.log(projectsArray);
+        updateShownProjects();
+    }
+})
+
+function callCreateTodo(){
+    let title = document.getElementById("createTitle");
+    let fecha = document.getElementById("createFecha");
+    let project = document.getElementById("createProject");
+    let priority = document.getElementById("createPriority");
+    let description = document.getElementById("createInfo");
+
+    return createTodo(title.value,description.value,new Date(fecha.value),priority.value,project.value);
+}
 
