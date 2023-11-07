53949e2aa22309fa701c3a70fc16826512b9e3ed
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-11-07 18:10:06 -0500

    Agrega forma de mostrar los distintos checks

--- src/updateTaskDisplay.js
+++ src/updateTaskDisplay.js
@@ -54,6 +54,51 @@ function updateChecklistDisplay(checkObj,modal){
     fecha.value = format(checkObj.dueDate, "yyyy-MM-dd'");;
     project.value = checkObj.project;
     priority.value = checkObj.priority;
+    addChecksContainer(checkObj);
+}
+
+function addChecksContainer(checkObj){
+    const checkContainer = document.getElementById("updateCheckContainer");
+    let checksArray = checkObj.checkList;
+    checksArray.forEach((check,index)=>{
+        checkContainer.appendChild(createChecksLabel(check,index));
+    })
+    return checkContainer
+}
+
+
+function createChecksLabel(checkObj,position){
+    const label = document.createElement("label");
+    label.setAttribute("for","check"+position);
+    label.setAttribute("data-position",position);
+    label.classList.add("sideLabel");
+
+    const paragraph = document.createElement("p");
+    paragraph.classList.add("checkLabel");
+    paragraph.textContent = "Check: "+(position+1);
+
+    label.appendChild(paragraph);
+    label.appendChild(createCheck(checkObj,position));
+    return label
+
+}
+
+function createCheck(checkObj,position){
+    const check = document.createElement("input");
+    check.setAttribute("type","text");
+    check.setAttribute("name","check"+position);
+    check.setAttribute("id","check"+position)
+    check.setAttribute("data-position",position)
+    check.classList.add("createCheck");
+    check.value = checkObj.title;
+    const addCheckEvent = function(e){
+        if(e.target.value.length >= 3){
+            const checkContainer = document.getElementById("createChecksContainer");
+            checkContainer.appendChild(createChecksLabel(Number(e.target.getAttribute("data-position"))+1));
+            e.target.removeEventListener("keyup",addCheckEvent);
+        }
+    }
+    return check;
 }
 
 function checkIFShowDescription(taskObj){
