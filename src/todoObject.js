767be27311ff5079303907103cf1c8f2a60344b6
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-11-11 08:12:37 -0500

    Agrega comentarios explicando modulo todoObject

--- src/todoObject.js
+++ src/todoObject.js
@@ -1,4 +1,12 @@
 import format from 'date-fns/format/index.js'
+
+/*todo es un objeto que almacena informacion de una todo.
+_title es su titulo
+_description es el texto de la note
+_dueDate es un date obj de la fecha limite del task
+priority la string de un numero (1 a 5),
+project la categoria a la que pertenece la note
+status indica si la nota fue completada o no*/
 const createTodo = (_title,_description,_dueDate,_priority,_project,_status=false)=>{
     const type = "todo";
     return{
@@ -50,7 +58,7 @@ const createTodo = (_title,_description,_dueDate,_priority,_project,_status=fals
              return type;
         },
         getFormatedDate : function(){
-
+            //Devuelve la fecha como un string en formato dd/mm/aa
             return format(_dueDate,"P");
         }
     }
